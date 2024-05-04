4879adff56357296a5b178898f1bcd6532069e5e
Rajat321singh <forselfrajat58615@gmail.com>
2024-05-04 15:14:17 +0530

    create Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 import './App.css'
+import Home from './pages/Home.jsx'
+import SignIn from './pages/SignIn.jsx'
+// import SignOut from './pages/SignOut'
+import SignUp from './pages/SignUp.jsx'
+import Profile from './pages/Profile.jsx'
+import About from './pages/About.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-     <h1 className='text-3xl'>Rajat singh</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
