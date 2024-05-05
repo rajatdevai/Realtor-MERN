b70d1e682dfc8d50c53a3be61cdc89e5fc94171f
Rajat321singh <forselfrajat58615@gmail.com>
2024-05-05 14:56:51 +0530

    user model

---
+++ Api/models/User.model.js
@@ -0,0 +1,23 @@
+import mongoose from'mongoose';
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        unique:true,
+        required:true 
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true 
+    }
+,
+password:{
+    type:String,
+    unique:true,
+    required:true 
+}
+},{timestamps:true});
+
+const User=mongoose.model("User",userSchema)
+
+export default userSchema;
